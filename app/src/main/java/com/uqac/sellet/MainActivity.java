804dfee7f5b107ed0e20b526bdae10773e46e7bc
Gableb <gableb@hotmail.fr>
2020-05-01 18:59:25 -0400

    Login activity

--- app/src/main/java/com/uqac/sellet/MainActivity.java
+++ app/src/main/java/com/uqac/sellet/MainActivity.java
@@ -1,14 +1,42 @@
 package com.uqac.sellet;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        updateUI(currentUser);
+        if(currentUser == null){
+            Intent intent = new Intent(this, EmailPasswordActivity.class);
+            startActivity(intent);
+        }
     }
 }
